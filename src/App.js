66f6566923b1ec9c0047920ea12de2e3a7d6dc54
Vontha Chan <89168934+vonthachan@users.noreply.github.com>
2025-09-02 20:03:25 -0500

    working on adding x and o

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React from "react";
 
-function Square() {
-  const [value, setValue] = React.useState(null);
+function Square({ value, onSquareClick }) {
   function handleClick() {
     setValue("X");
   }
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
